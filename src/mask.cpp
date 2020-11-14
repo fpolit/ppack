cffede23b2515141b6115cc6aeffa3361d174902
glozanoa <gacuna@protonmail.com>
2020-11-14 17:57:19 -0500

    (SOLVE BUG)Calling the string constructor in the preinicialization of mask contructor

--- src/mask.cpp
+++ src/mask.cpp
@@ -1,3 +1,10 @@
+
+#ifndef __INCLUDE_STD_IOSTREAM_H__
+#define __INCLUDE_STD_IOSTREAM_H__
+#include <iostream>
+#endif //__INCLUDE_STD_IOSTREAM_H__
+
+
 #ifndef __INCLUDE_MASK_H__
 #define __INCLUDE_MASK_H__
 #include "../include/mask.hpp"
@@ -9,40 +16,60 @@
 #endif //__INCLUDE_CHARACTER_H__
 
 
-#include "omp.h"
+#ifndef __INCLUDE_STD_IOSTREAM_H___
+#define __INCLUDE_STD_IOSTREAM_H__
+#include<iostream>
+#endif //__INCLUDE_STD_IOSTREAM_H__
+
+using namespace std;
+
+//#include "omp.h"
 
 Character characters;
+//string maskSymbols = characters.masksymbol
 
 bool ismask(string mask)
 {
     bool master_ismask = true;
-    #pragma omp parallel for shared(master_ismask) 
+    string maskSymbols = characters.masksymbols;
+    //#pragma omp parallel for shared(master_ismask, maskSymbols)
     for(int k=1; k<mask.size(); k+=2)
     {
-        string maskSymbols = characters.masksymbols;
+
 
         // if the string mask  isn't a mask, then only pass.
         if(!master_ismask)  continue;
 
         if(maskSymbols.find(mask[k], 0) == string::npos)
         {
-            #pragma omp critical
+          //#pragma omp critical
                 master_ismask = false;
-        }         
+        }
     }
-    
     return master_ismask;
 }
 
+bool ismasksymbol(string masksymbol) // check if a symbols is a valid symbol mask
+{
+  if(masksymbol.size() == 2)
+    {
+      string maskSymbols = characters.masksymbols;
+      if(maskSymbols.find(masksymbol[1], 0) == string::npos)
+        return true;
+    }
+  return false;
 
+}
 // START: Mask class implementation
 
 
 Mask::Mask(string mask)
+  :string(mask) // NOTA: EL CONTRUCTOR DE LA CLASE PADRE (string) DEBE IR AQUI NO EN EL CUERPO SINO NO FUNCIONA.
 {
   //verify that mask have the format of a MASK
   if(ismask(mask)){
-    string(mask);
+    //string(mask);
+    cout << mask << " is a mask." << endl;
   }
   else{
     throw "Invalid mask: " + mask;
@@ -50,21 +77,39 @@ Mask::Mask(string mask)
 }
 
 
-size_t Mask::size() const
+void Mask::expand(string masksymbol)
 {
-  //    string mask_value = *this;
-  //return mask_value.size()/2;
-  return this->size()/2;
+  if(ismasksymbol(masksymbol))
+    *this += masksymbol;
+  else
+    cout << masksymbol << " isn't a symbol mask." << endl;
 }
 
-// bool Mask::ismasksymbol(string letther)
+// vector<Mask> Mask::permutations()
 // {
-//     string maskSymbols = characters.masksymbols;
-//     if (maskSymbols.find(letther) == string::npos)
-//     {
-//         return false;
-//     }
-//     return true;
+//   // perform all the permutation of the mask
+//   cout << "No forget implement permutation method(Mask Class)." << endl;
 // }
 
+// maskStruct maskAnalysis(Mask mask)
+// {
+//   maskStruct mask_struct;
+// #pragma omp parallel for shared(mask)
+//   for(int i=1; i<mask.size(); i+=2)
+//     {
+//     switch(mask[i])
+//       {
+//       case 'l':
+//         mask_struct.lowercase += 1;
+//       case "d":
+//         mask_struct.digit += 1;
+//       case "u":
+//         mask_struct.uppercase += 1;
+//       case "s":
+//         mask_struct.special += 1;
+//       }
+//     }
+
+//   return mask_struct;
+// }
 // END: Mask class implementation
