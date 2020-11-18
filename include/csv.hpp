65db7e9eb900698f90443914c13e533a739f7018
glozanoa <gacuna@protonmail.com>
2020-11-18 09:50:59 -0500

    addiong CSVReader class

---
+++ include/csv.hpp
@@ -0,0 +1,45 @@
+#ifndef __CSV_H__
+#define __CSV_H__
+
+#ifndef __INCLUDE_STD_STRING_H__
+#define __INCLUDE_STD_STRING_H__
+#include<string>
+#endif //__INCLUDE_STD_STRING_H_
+
+#ifndef __INCLUDE_STD_VECTOR_H__
+#define __INCLUDE_STD_VECTOR_H__
+#include<vector>
+#endif // __INCLUDE_STD_VECTOR_H_
+
+
+// #ifndef __INCLUDE_FSTREAM_H__
+// #define __INCLUDE_FSTREAM_H__
+// #include<fstream>
+// #endif //__INCLUDE_FSTREAM_H__
+
+
+// #ifndef __INCLUDE_ALGORITHM_H__
+// #define __INCLUDE_ALGORITHM_H__
+// #include<algorithm>
+// #endif //__INCLUDE_ALGORITHM_H__
+
+
+
+class CSVReader
+/*
+ * A class to read data from a csv file.
+ */
+{
+private:
+    std::string fileName;
+    std::string delimeter;
+public:
+    CSVReader(std::string filename, std::string delm = ",") :
+            fileName(filename), delimeter(delm)
+    { }
+    // Function to fetch data from a CSV File
+    std::vector<std::vector<std::string>> getData();
+};
+
+
+#endif //__CSV_H__
---
+++ src/csv.cpp
@@ -0,0 +1,46 @@
+
+#ifndef __INCLUDE_STD_STRING_H__
+#define __INCLUDE_STD_STRING_H__
+#include<string>
+#endif //__INCLUDE_STD_STRING_H_
+
+#ifndef __INCLUDE_STD_VECTOR_H__
+#define __INCLUDE_STD_VECTOR_H__
+#include<vector>
+#endif // __INCLUDE_STD_VECTOR_H_
+
+
+#ifndef __INCLUDE_FSTREAM_H__
+#define __INCLUDE_FSTREAM_H__
+#include<fstream>
+#endif //__INCLUDE_FSTREAM_H__
+
+
+#ifndef __INCLUDE_ALGORITHM_H__
+#define __INCLUDE_ALGORITHM_H__
+#include<algorithm>
+#endif //__INCLUDE_ALGORITHM_H__
+
+#include <boost/algorithm/string.hpp>
+
+
+std::vector<std::vector<std::string>> CSVReader::getData()
+/*
+ * Parses through csv file line by line and returns the data
+ * in vector of vector of strings.
+ */
+{
+  std::ifstream file(fileName);
+  std::vector<std::vector<std::string> > dataList;
+  std::string line = "";
+  // Iterate through each line and split the content using delimeter
+  while (getline(file, line))
+    {
+      std::vector<std::string> vec;
+      boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
+      dataList.push_back(vec);
+    }
+  // Close the File
+  file.close();
+  return dataList;
+}
