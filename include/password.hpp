c3be8e1a2e765f7f6de8f67d1b6f349b0123884f
glozanoa <gacuna@protonmail.com>
2020-11-23 18:21:10 -0500

    reimplementation password class

--- include/password.hpp
+++ include/password.hpp
@@ -1,10 +1,13 @@
+/*
+ * Password class definition
+ * Password class reimplemented 23 nov 20202
+ *
+ * Maintainer: glozanoa <glozanoa@uni.pe>
+ */
+
+
 #ifndef __PASSWORD_H__
 #define __PASSWORD_H__
-
-#ifndef __INCLUDE_STD_MAP_H_
-#define __INCLUDE_STD_MAP_H_
-#include<map>
-#endif // __INCLUDE_STD_MAP_H_
 
 #ifndef __INCLUDE_STD_STRING_H_
 #define __INCLUDE_STD_STRING_H_
@@ -13,128 +16,34 @@
 
 using namespace std;
 
+
 #ifndef __INCLUDE_MASK_H__
 #define __INCLUDE_MASK_H__
 #include "mask.hpp"
 #endif // __INCLUDE_MASK_H__
 
 
-
-
-typedef enum SCS {
-// base types
-    loweralpha, 
-    upperalpha,
-    numeric,
-    special,
-    
-//common derived combination (unordered) 
-    alpha,              // (unordered)[loweralpha + upperalpha]
-    
-    mixalphaspecial,    // (unordered)[loweralpha + upperalpha + special]
-    mixalphanum,        // (unordered)[loweralpha + upperalpha + numeric]
-    mixspecialnum,      // (unordered)[special + numeric]
-    mixall,             // (unordered)[alpha, numeric, special]
-    none                // default value to empty SimpleCharSet variable
-}SCS;
-
-typedef enum ACS{
-//more advance combinations (ordered)
-
-    //combination of alpha(lower, upper) + special
-    alphaspecial,       // [(unordered)(loweralpha + upperalpha)] + special
-    loweralphaspecial,  // loweralpha + special
-    upperalphaspecial,  // upperalpha + special
-
-    specialalpha,       // special + [(unordered)(loweralpha + upperalpha)]
-    specialloweralpha,  // special + loweralpha
-    specialupperalpha,  // special + upperalpha
-    
-    // combination of alpha(lower, upper) + numeric
-
-    alphanum,           // [(unordered)(loweralpha + upperalpha)] + numeric
-    loweralphanum,      // loweralpha + numeric
-    upperalphanum,      // upperalpha + numeric
-
-    // combination of special + numeric
-
-    specialnum,         // special + numeric
-    numspecial,         // numeric + special
-    none                // default value to empty AdvanceCharSet variable
-} ACS;
-
-// Values Simples Character Set
-map<SCS, string> SimpleCharSet = {
-    {SCS::loweralpha, "loweralpha"},
-    {SCS::upperalpha,"upperalpha"},
-    {SCS::numeric,"numeric"},
-    {SCS::special,"special"},
-    {SCS::alpha,"alpha"},
-    {SCS::mixalphaspecial,"mixalphaspecial"},
-    {SCS::mixalphanum, "mixalphanum"},
-    {SCS::mixspecialnum, "mixspecialnum"},
-    {SCS::mixall, "mixall"},
-    {SCS::none, "none"}
-};
-
-
-map<ACS, string> AdvanceCharSet = {
-    {ACS::alphaspecial, "alphaspecial"},
-    {ACS::loweralphaspecial, "loweralphaspecial"},
-    {ACS::upperalphaspecial, "upperalphaspecial"},
-    {ACS::specialalpha, "specialalpha"},
-    {ACS::specialloweralpha, "specialloweralpha"},
-    {ACS::specialupperalpha, "specialupperalpha"},
-    {ACS::alphanum, "alphanum"},
-    {ACS::loweralphanum, "loweralphanum"},
-    {ACS::upperalphanum, "upperalphanum"},
-    {ACS::specialnum, "specialnum"},
-    {ACS::numspecial, "numspecial"},
-    {ACS::none, "none"}
-};
-
-
-
-typedef struct PStruct{
-        unsigned int lowercase  = 0;
-        unsigned int uppercase  = 0;
-        unsigned int special    = 0;
-        unsigned int digit      = 0;
-}PStruct;// Password Structure
-
-
-
-Mask analyzePassword(string passwd, PStruct *pStructx);
-// determine password's SCS
-SCS detSimpleCS(PStruct pStruct);
-
+Mask passwdAnalysis(string passwd); //analyze the string passwd and get its mask
 
 class Password : public string
 {
 private:
     Mask mask;
-    int complexity=-1;
-
-    // Charater set password
-    SCS simple;
-    //optional no computer in contructor (default value nane(ACS))
-    ACS advance;
-
-    PStruct passwdStruct;
-
 
 public:
     Password(string passwd);
-    ~Password(){}
+  ~Password(){};
 
-    //string simpleMask(); // remplaced by charset
-    string advanceMask(){return mask;}
-    // return a type of SCS (map). 
-    string scs(); 
-    // return a type of ACS (map). 
-    string acs();
-  //    int complexity();
 
-  Mask getMask(){return mask;}
+  // get and set methods
+  int getComplexity(){return 0;} // NOTA: IMPLEMENTAR LA COMPLEJIDAD DE UNA MASCARA
+  SCS getSCS(){return mask.getSCS();}
+  ACS getACS(){return mask.getACS();}
+
+  void setComplexity(int complex){mask.setComplexity(complex);}
+  void setACS(ACS asv){mask.setACS(asv);}
+  void setSCS(SCS scs){mask.setSCS(scs);}
+
+  string getMask(){return mask;}
 };
 #endif // __PASSWORD_H__
--- src/password.cpp
+++ src/password.cpp
@@ -1,3 +1,19 @@
+/*
+ * Password class implementarion
+ * Password class reimplemented 23 nov 20202
+ *
+ * Maintainer: glozanoa <glozanoa@uni.pe>
+ */
+
+
+
+#ifndef __INCLUDE_STD_STRING_H__
+#define __INCLUDE_STD_STRING_H__
+#include<string>
+#endif //__INCLUDE_STD_STRING_H__
+
+using namespace std;
+
 #ifndef __INCLUDE_PASSWORD_H__
 #define __INCLUDE_PASSWORD_H__
 #include "../include/password.hpp"
@@ -19,131 +35,91 @@
 #endif //__INCLUDE_OMP_H__
 
 
-// BEGIN: Password class implementation
 
-Password::Password(string passwd)
-  :string(passwd)
+SCS scsParser(maskStruct mstruct)
 {
-  Mask mask_passwd = analyzePassword(passwd, &passwdStruct);
-  mask = mask_passwd;
-  simple = detSimpleCS(passwdStruct);
-  advance = ACS::none;
-}
+    // parse a maskStruct to determine the scs of the mask
 
-
-string Password::scs()
-{
-    return SimpleCharSet[simple];    
-}
-
-// string Password::acs()
-// {
-// // if (characters != "")
-// //     return characters;
-// // else
-// // {
-// //     LetterMask symbolsMask;
-// //     map<string, unsigned int> simple{
-// //         {"lowercase", 0},
-// //         {"uppercase", 0},
-// //         {"special", 0},
-// //         {"digit", 0}
-// //     };
-// //     for(int k=1; k<2*mask.size(); k+=2)
-// //     {
-// //         string letter_mask = to_string(mask[k]);
-// //         if(letter_mask == symbolsMask.lowercase)
-// //             simple["lowercase"] += 1;
-
-// //     }
-// // }
-// }
-
-// int Password::complexity()
-// {
-//   return 0;
-// }
-
-// END: Password class implementation
-
-
-// BEGIN: analyzePassword function implementation
-Mask analyzePassword(string passwd, PStruct *pStruct)
-{
-    string master_mask = "";
-    maskSymbols symbols;
-    #pragma omp parallel for ordered \
-        shared(master_mask, passwd, symbols, passwdStruct)
-    for(int k=0; k<passwd.size(); k++)
-    {
-        #pragma omp ordered
-        {
-            string letther = to_string(passwd[k]);
-
-            if(islower(letther))
-            {
-                master_mask += symbols.lowercase;
-                pStruct->lowercase += 1;
-            }
-            else if(isupper(letther))
-            {
-                master_mask += symbols.uppercase;
-                pStruct->uppercase += 1;
-            }
-            else if(isdigit(letther))
-            {
-                master_mask += symbols.digit;
-                pStruct->digit += 1;
-            }
-            else
-            {
-                master_mask += symbols.special;
-                pStruct->special += 1;
-            }
-        }
-    }
-    return Mask(master_mask);
-}
-// END: analyzePassword function implementation
-
-// END: setSimpleCS function implementation
-
-SCS detSimpleCS(PStruct pStruct)
-{
-    // determine the set of characteres that confor the password (simple Character Set)
-    
-    
-    if(pStruct.lowercase &&
-        !(pStruct.digit || pStruct.uppercase || pStruct.special))
+    if(mstruct.lowercase &&
+        !(mstruct.digit || mstruct.uppercase || mstruct.special))
         return SCS::loweralpha;
 
-    else if(pStruct.uppercase &&
-        !(pStruct.lowercase || pStruct.digit || pStruct.special))
+    else if(mstruct.uppercase &&
+        !(mstruct.lowercase || mstruct.digit || mstruct.special))
         return SCS::numeric;
 
-    else if(pStruct.digit &&
-        !(pStruct.lowercase || pStruct.uppercase || pStruct.special))
+    else if(mstruct.digit &&
+        !(mstruct.lowercase || mstruct.uppercase || mstruct.special))
         return SCS::numeric;
 
-    else if(pStruct.special &&
-        !(pStruct.lowercase || pStruct.uppercase || pStruct.digit))
+    else if(mstruct.special &&
+        !(mstruct.lowercase || mstruct.uppercase || mstruct.digit))
         return SCS::numeric;
 
-    else if((pStruct.uppercase && pStruct.lowercase) &&
-        !(pStruct.uppercase || pStruct.digit))
+    else if((mstruct.uppercase && mstruct.lowercase) &&
+        !(mstruct.uppercase || mstruct.digit))
         return SCS::alpha;
-    
-    else if((pStruct.special && pStruct.uppercase && pStruct.lowercase) &&
-        !(pStruct.digit))
+
+    else if((mstruct.special && mstruct.uppercase && mstruct.lowercase) &&
+        !(mstruct.digit))
         return SCS::mixalphaspecial;
 
-    else if((pStruct.digit && pStruct.special) &&
-        !(pStruct.uppercase || pStruct.lowercase))
+    else if((mstruct.digit && mstruct.special) &&
+        !(mstruct.uppercase || mstruct.lowercase))
         return SCS::mixspecialnum;
-    
-    else if(pStruct.special && pStruct.lowercase && pStruct.uppercase && pStruct.digit)
+
+    else if(mstruct.special && mstruct.lowercase && mstruct.uppercase && mstruct.digit)
         return SCS::mixall;
 
     else
         return SCS::none;
+}
+
+
+Mask  analyzePassword(string passwd)
+{
+    string master_mask = "";
+
+    Mask maskParser(master_mask);
+
+    // computer the mask of the password and fill the maskStruct
+    // #pragma omp parallel for ordered \
+    //     shared(master_mask, passwd, symbols, passwdStruct)
+    for(int k=0; k<passwd.size(); k++)
+    {
+      //#pragma omp ordered
+        {
+            string letther = to_string(passwd[k]);
+
+            if(CharSet.islower(letther))
+            {
+              maskParser.realloc("?l");
+            }
+            else if(isupper(letther))
+            {
+              maskParser.realloc("?u");
+            }
+            else if(isdigit(letther))
+            {
+              maskParser.realloc("?d");
+            }
+            else //the character is a special character
+            {
+              maskParser.realloc("?s");
+            }
+        }
+    }
+    maskStruct mstruct = maskParser.getStruct();
+    maskParser.setSCS(scsParser(mstruct));
+
+    return maskParser;
+}
+
+
+
+
+Password::Password(string passwd)
+  :string(passwd)
+{
+  mask = analyzePassword(passwd);
 }
