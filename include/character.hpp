62442798a5cd6169e0b99ee500443eb89a86eaa3
glozanoa <gacuna@protonmail.com>
2020-11-23 22:25:19 -0500

    defining charset namespace

--- include/character.hpp
+++ include/character.hpp
@@ -1,3 +1,10 @@
+/*
+ * charset namespace definition
+ * character head file reimplemented 23 nov 20202
+ *
+ * Maintainer: glozanoa <glozanoa@uni.pe>
+ */
+
 #ifndef __CHARACTER_H__
 #define __CHARACTER_H__
 
@@ -9,21 +16,53 @@
 
 using namespace std;
 
-typedef struct Character
+namespace charset
 {
-    string ascci_letters =  R"(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ)";
-    string ascii_lowercase = R"(abcdefghijklmnopqrstuvwxyz)";
-    string ascii_uppercase = R"(ABCDEFGHIJKLMNOPQRSTUVWXYZ)";
-    string digits = R"(0123456789)";
-    string special = R"(!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)";
-    string printable = R"(0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c)";
-    string whitespace = R"( \t\n\r\x0b\x0c)";
-    string masksymbols = R"(ludsa)";
-}Character;
+  string ascci_letters =  R"(abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ)";
+  string ascii_lowercase = R"(abcdefghijklmnopqrstuvwxyz)";
+  string ascii_uppercase = R"(ABCDEFGHIJKLMNOPQRSTUVWXYZ)";
+  string digits = R"(0123456789)";
+  string special = R"(!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)";
+  string printable = R"(0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c)";
+  string whitespace = R"( \t\n\r\x0b\x0c)";
+  string masksymbols = R"(ludsa)";
 
-bool islower(string letther);
-bool isupper(string letther);
-bool isdigit(string letther);
-bool isspecial(string letther);
+
+  bool isdigit(string letther)
+  {
+    if (digits.find(letther) == string::npos)
+      {
+        return false;
+      }
+    return true;
+  }
+
+  bool islower(string letther)
+  {
+    if (ascii_lowercase.find(letther) == string::npos)
+      {
+        return false;
+      }
+    return true;
+  }
+
+  bool isupper(string letther)
+  {
+    if (ascii_uppercase.find(letther) == string::npos)
+      {
+        return false;
+      }
+    return true;
+  }
+
+  bool isspecial(string letther)
+  {
+    if (special.find(letther) == string::npos)
+      {
+        return false;
+      }
+    return true;
+  }
+}
 
 #endif // __CHARACTER_H__
